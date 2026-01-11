80d9dd8c0be9e1c79d4692566c36bc670902a25e
amiljoseph03 <amiljoseph03@gmail.com>
2026-01-11 21:36:43 +0530

    login page done

---
+++ src/app/login/login.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  constructor(private uv: Router) {}
+
+  login() {
+    this.uv.navigate(['/dashboard'])
+  }
+}
